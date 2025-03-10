1865f6a625ec44391a475a43e239891d9f7a4bb2
Pavel Kovalev <bunak11@ya.ru>
2025-03-10 20:27:38 +0300

    replacement for JX and React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 3015.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>
-					<p>{currentYear}</p>
-				</div>
-			</header>
-		</div>
-	);
-}
+  const currentYear = new Date().getFullYear();  // Получаем текущий год
 
-
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      'header',
+      { className: 'App-header' },
+      React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+      React.createElement(
+        'p',
+        null,
+        'Edit ',
+        React.createElement('code', null, 'src/App.js'),
+        ' and save to reload 3015.'
+      ),
+      React.createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer'
+        },
+        'Learn React'
+      ),
+      React.createElement(
+        'div',
+        null,
+        React.createElement('p', null, `© ${currentYear}`)
+      )
+    )
+  );
+};
