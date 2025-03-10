eba89387c2f20a539e159cb276f1fdf2441b119e
Pavel Kovalev <bunak11@ya.ru>
2025-03-10 19:58:36 +0300

    add currentYear JSX

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+	const currentYear = new Date().getFullYear();
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload 3015.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<div>
+					<p>{currentYear}</p>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+
